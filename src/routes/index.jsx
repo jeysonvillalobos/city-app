14f2b4738af405f69b45415e8718a9339450fa3c
Jeyson Villalobos <jeyson.villalobos@autodesk.com>
2022-09-22 09:49:04 -0500

    Updates

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, Route } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../pages/home';
 
